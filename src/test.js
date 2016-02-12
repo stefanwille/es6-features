ec0fe233e5c5d661cf8e8ebb2b050e76d15118b4
Stefan Wille <github@stefanwille.com>
2016-02-12 12:40:34 +0100

    Add first version

---
+++ src/test.js
@@ -0,0 +1,33 @@
+var assert = require('assert')
+
+import 'babel-polyfill'
+import 'babel-runtime/regenerator'
+
+
+function* greet() {
+  yield "hello"
+  yield "bienvenido"
+  yield "ciao"
+}
+
+describe('greet generator function', () => {
+  it('generates several greetings', () => {
+    const greeter = greet()
+
+    const result0 = greeter.next()
+    assert.equal (false, result0.done)
+    assert.equal ('hello', result0.value)
+
+    const result1 = greeter.next()
+    assert.equal (false, result1.done)
+    assert.equal ('bienvenido', result1.value)
+
+    const result2 = greeter.next()
+    assert.equal (false, result2.done)
+    assert.equal ('ciao', result2.value)
+
+    const result3 = greeter.next()
+    assert.equal (true, result3.done)
+    assert.equal (undefined, result3.value)
+  })
+})
