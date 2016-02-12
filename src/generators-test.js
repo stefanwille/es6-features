571931b2d6a24effc07a3791be427edc47876690
Stefan Wille <github@stefanwille.com>
2016-02-12 13:44:58 +0100

    Add for..of

--- src/generators-test.js
+++ src/generators-test.js
@@ -4,29 +4,38 @@ import 'babel-runtime/regenerator'
 import assert from 'assert'
 
 function* greet() {
-  yield "hello"
-  yield "bienvenido"
-  yield "ciao"
+  yield 'hello'
+  yield 'bienvenido'
+  yield 'ciao'
 }
 
 describe('generator function', () => {
-  it('generates several results', () => {
-    const greeter = greet()
+  it('returns an iterator, which in turn returns iterator-results', () => {
+    const iterator = greet()
 
-    const result0 = greeter.next()
-    assert.equal (false, result0.done)
-    assert.equal ('hello', result0.value)
+    const iteratorResult0 = iterator.next()
+    assert.equal (false, iteratorResult0.done)
+    assert.equal ('hello', iteratorResult0.value)
 
-    const result1 = greeter.next()
-    assert.equal (false, result1.done)
-    assert.equal ('bienvenido', result1.value)
+    const iteratorResult1 = iterator.next()
+    assert.equal (false, iteratorResult1.done)
+    assert.equal ('bienvenido', iteratorResult1.value)
 
-    const result2 = greeter.next()
-    assert.equal (false, result2.done)
-    assert.equal ('ciao', result2.value)
+    const iteratorResult2 = iterator.next()
+    assert.equal (false, iteratorResult2.done)
+    assert.equal ('ciao', iteratorResult2.value)
 
-    const result3 = greeter.next()
-    assert.equal (true, result3.done)
-    assert.equal (undefined, result3.value)
+    const iteratorResult3 = iterator.next()
+    assert.equal (true, iteratorResult3.done)
+    assert.equal (undefined, iteratorResult3.value)
+  })
+
+  it('returns a result that can be iterated with for..of', () => {
+    const iterator = greet()
+    const allValues = []
+    for(let n of iterator) {
+      allValues.push(n)
+    }
+    assert.deepEqual(['hello', 'bienvenido', 'ciao'], allValues)
   })
 })
