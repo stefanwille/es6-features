852bf86f3cee870729f8d9dea7b043aa19ea0556
Stefan Wille <github@stefanwille.com>
2016-02-12 13:39:28 +0100

    Add classes

---
+++ src/class-test.js
@@ -0,0 +1,35 @@
+import assert from 'assert'
+
+describe('class syntax', () => {
+  it('methods', () => {
+    class Person {
+      name () {
+        return 'John Smith'
+      }
+    }
+    assert.equal('John Smith', new Person().name())
+  })
+
+  it('properties', () => {
+    class Person {
+      get drink() {
+        return this._drink
+      }
+
+      set drink(v) {
+        this._drink = v
+      }
+    }
+    const person = new Person()
+    person.drink = 'Coffee'
+    assert.equal('Coffee', person.drink)
+  })
+
+  it('static', () => {
+    class Person {
+      static INITIAL_STATE = 'born'
+    }
+
+    assert.equal('born', Person.INITIAL_STATE)
+  })
+})
